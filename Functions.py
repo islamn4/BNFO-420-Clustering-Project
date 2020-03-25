2df71a68e420d419faa711af79c30ac41f23721c
SweeetSteve <46939312+SweeetSteve@users.noreply.github.com>
2020-03-25 13:19:59 -0400

    Most of the code is done. It needs testing

--- Functions.py
+++ Functions.py
@@ -40,6 +40,44 @@ def ambiguous_aa(seq):
         return True
 
 
+def H5N1_subtype_check(strain_header):
+    """
+        A function that determines if the virus strain in question is an H5N1 virus or a different subtype.
+
+        :parameter
+        strain_header : string
+            Contains the fasta header of an Influenza A virus.
+        :return:
+        True if the virus strain is H5N1 strain.
+        False if the virus strain is not an H5N1 strain.
+        """
+
+    if(strain_header.find("H5N1") != -1):
+        return True
+    else:
+        return False
+
+
+def subtype_filter(strain_header):
+    """
+        
+    """
+
+    if(strain_header.find("H5N1") != -1):
+        return "H5N1"
+    elif(strain_header.find("H9N2") != -1):
+        return "H9N2"
+    elif(strain_header.find("H1N1") != -1):
+        return "H1N1"
+    elif(strain_header.find("H3N2") != -1):
+        return "H3N2"
+    else:
+        return "none"
+
+
+
+
+
 
 
 
